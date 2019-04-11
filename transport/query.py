3a3d8a75c0a688e270783d7e44b42a58b74c0379
James Williams <29534093+williajm@users.noreply.github.com>
2019-04-11 22:38:30 +0100

    Sanitise the transport API query result
    Allows easier use of different query sources in the future.
    Reduces the amount of data sent over the websocket.

--- transport/query.py
+++ transport/query.py
@@ -1,14 +1,33 @@
 """
 blah
 """
-import logging
+from dataclasses import dataclass
 import json
+import logging
+from typing import List
 import requests
 from config import config
 
 log = logging.getLogger(__name__)
 session = requests.session()
 session.params = config.get('auth')
+
+
+@dataclass()
+class Train:
+    aimed_departure_time: str
+    expected_departure_time: str
+    destination_name: str
+    platform: str
+    operator_name: str
+    origin_name: str
+    status: str
+
+
+class TrainEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        return obj.__dict__
 
 
 def get_train_live(station: str) -> str:
@@ -22,5 +41,24 @@ def get_train_live(station: str) -> str:
     resp = session.get(url)
     if resp.status_code != requests.codes.ok:
         log.warning(f'http status={resp.status_code} for {url}')
-    log.debug(json.dumps(json.loads(resp.text), indent=4))
-    return resp.text
+        return '' # TODO throw an exception
+    sanitised_obj = _sanitise(resp.text)
+    sanitised_json = json.dumps(sanitised_obj, indent=4, cls=TrainEncoder)
+    log.debug(f'sanitised_json={sanitised_json}')
+    return sanitised_json
+
+
+def _sanitise(query_result: str) -> List[Train]:
+    query_json = json.loads(query_result)
+    departures = query_json.get('departures', {}).get('all', [])
+    trains = []
+    for departure in departures:
+        train = Train(aimed_departure_time=departure.get('aimed_departure_time'),
+                      expected_departure_time=departure.get('expected_departure_time'),
+                      destination_name=departure.get('destination_name'),
+                      platform=departure.get('platform'),
+                      operator_name=departure.get('operator_name'),
+                      origin_name=departure.get('origin_name'),
+                      status=departure.get('status'))
+        trains.append(train)
+    return trains
