27f7be83545d6eb4a0ece434a0c9be2109b43fb0
James Williams <29534093+williajm@users.noreply.github.com>
2019-04-14 11:54:56 +0100

    Fix reported pylint errors
    False positives added to ignore list.
    Use pytest fixture in unit tests.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: lint
 
 lint:
-	pylint --disable=W1203,C0103,E1101 transport/ tests/*.py
+	pylint --disable=W1203,C0103,E1101,W0621,E1135,E1136,E1137,E0202 transport/ tests/*.py
--- transport/app.py
+++ transport/app.py
@@ -1,70 +1,106 @@
+"""
+The main module for this flask application.
+Sets up routes and provides AJAX and websocket methods.
+"""
 import csv
+from dataclasses import dataclass
+from importlib import resources
+import logging.config
 import os
 from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO, emit
 from transport.query import get_train_live
-import logging
-import logging.config
-from importlib import resources
 from transport.trie import Trie
 
 log = logging.getLogger(__name__)
 app = Flask(__name__)
-# TODO add secret
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = os.getenv('ftu_secret_key', 'secret')
 bootstrap = Bootstrap(app)
 socket_io = SocketIO(app)
 
 
 @app.route('/')
 def get_train_times():
+    """
+    Render the initial page
+    """
     log.info(f'request={request}')
     return render_template('train_times.html')
 
 
 @app.route('/autocomplete', methods=['GET'])
 def autocomplete():
+    """
+    Searches the trie for autocomplete suggestions.
+    """
     log.info(f'request={request}')
     query = request.args.get('q')
     suggestions = []
-    for suggestion in trie.search(query):
-        suggestions.append(suggestion[0]) # TODO be more defensive
+    for station in trie.search(query):
+        suggestions.append(station.name)
     log.info(f'autocomplete results={suggestions}')
     return jsonify(matching_results=suggestions)
 
 
 @socket_io.on('connect', namespace='/train')
 def train_connect():
+    """
+    Called when a websocket client connects.
+    """
     log.info('train_connect() called')
 
 
 @socket_io.on('disconnect', namespace='/train')
 def train_disconnect():
+    """
+    Called when a websocket client disconnects.
+    """
     log.info(f'train_disconnect() called')
 
 
 @socket_io.on('search_event', namespace='/train')
 def train_search(message):
-    log.info('received message: ' + str(message))
-    search_result = trie.search(message.get('station'))  # TODO sanitise
+    """
+    Returns the live departure times for a particular station.
+    """
+    log.info(f'received message: {message}')
+    search_result = trie.search(message.get('station'))
     log.info(f'search_result={search_result}')
-    trains = get_train_live(search_result[0][1])
-    emit('train_result', trains, json=True)
+    if search_result:
+        trains = get_train_live(search_result[0].code)
+        emit('train_result', trains, json=True)
 
 
 @socket_io.on_error_default
 def default_error_handler(e):
+    """
+    Called when a websocket error occurs.
+    """
     log.exception(e)
 
 
+@dataclass(frozen=True)
+class Station:
+    """
+    Station name to CRS code mapping. Stored as a value in the trie.
+    """
+    name: str
+    code: str
+
+
 def create_trie() -> Trie:
+    """
+    Create a Trie data structure from the packaged csv file.
+    :return: the trie
+    """
     trie = Trie()
     with resources.open_text('transport', 'station_codes.csv') as stations:
         rows = csv.reader(stations)
-        next(rows)
+        next(rows)  # skip the header
         for row in rows:
-            trie.insert(row[0], row)
+            station = Station(name=row[0], code=row[1])
+            trie.insert(key=station.name, value=station)
     return trie
 
 
