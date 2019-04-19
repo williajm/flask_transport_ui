f84dab23057a13d5b5ea68bfd74a19bb5bb2cf88
James Williams <29534093+williajm@users.noreply.github.com>
2019-04-19 14:22:23 +0100

    Parametrise integration tests
    Use pytests parametrise functionality to increase coverage.

--- tests/integration/test_ws.py
+++ tests/integration/test_ws.py
@@ -55,8 +55,12 @@ def test_disconnect(sio):
     assert not CustomNamespace.connected
 
 
-def test_search(sio):
-    sio.emit('search_event', {'station': 'Southampton Central'}, namespace='/train')
+@pytest.mark.parametrize(
+    'station_search, station_result',
+    [('Southampton Central', 'SOU'),
+     ('Glasgow Central', 'GLC')])
+def test_search(sio, station_search, station_result):
+    sio.emit('search_event', {'station': station_search}, namespace='/train')
     sio.sleep(1)
     assert len(CustomNamespace.train_result) == 1
     result = json.loads(CustomNamespace.train_result[0])
@@ -65,8 +69,17 @@ def test_search(sio):
         assert len(departure.keys()) == 7
         assert re.match(pattern='^[0-2][0-9]:[0:5][0:9]$', string=departure.get('aimed_departure_time'))
         assert re.match(pattern='^[0-2][0-9]:[0:5][0:9]$', string=departure.get('expected_departure_time'))
-        assert departure.get('destination_name').startswith('SOU destination')
+        assert departure.get('destination_name').startswith(f'{station_result} destination')
         assert re.match(pattern='^[0-1]?[0-9]$', string=departure.get('platform'))
-        assert departure.get('operator_name') == 'SOU operator'
-        assert departure.get('origin_name').startswith('SOU origin')
+        assert departure.get('operator_name') == f'{station_result} operator'
+        assert departure.get('origin_name').startswith(f'{station_result} origin')
         assert re.match(pattern='^(ON TIME|CANCELLED)$', string=departure.get('status'))
+
+
+@pytest.mark.parametrize('station_name',
+                         ['Southampton Central',
+                          'Glasgow Central'])
+def test_search_station_name(sio, station_name):
+    sio.emit('search_event', {'station': station_name}, namespace='/train')
+    sio.sleep(1)
+    assert CustomNamespace.station_name == station_name
