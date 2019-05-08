17c70cca91e0e538c2ef49072b525b8e43152230
James Williams <29534093+williajm@users.noreply.github.com>
2019-05-08 21:49:46 +0100

    Add first selenium tests
    Added a scenario outline to test autocomplete functionality.

---
+++ tests/ui/environment.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+
+def before_all(context):
+    driver = webdriver.Remote(
+        command_executor='http://127.0.0.1:4444/wd/hub',
+        desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True})
+    context.driver = driver
+
+
+def after_all(context):
+    context.driver.close()
