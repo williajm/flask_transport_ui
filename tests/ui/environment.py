3ac27b2dd3753e75ffc2450482656d9f87a8bdb7
James Williams <29534093+williajm@users.noreply.github.com>
2025-10-22 19:10:35 +0100

    Add selenium workflow (#62)

--- tests/ui/environment.py
+++ tests/ui/environment.py
@@ -1,13 +1,27 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
 def before_all(context):
     options = Options()
-    driver = webdriver.Remote(
-        command_executor="http://127.0.0.1:4444/wd/hub",
-        options=options,
-    )
+
+    # Detect if running in CI environment
+    if os.getenv("CI"):
+        # GitHub Actions: use local Chrome with headless mode
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        driver = webdriver.Chrome(options=options)
+    else:
+        # Local development: use Selenium Grid (docker)
+        driver = webdriver.Remote(
+            command_executor="http://127.0.0.1:4444/wd/hub",
+            options=options,
+        )
+
     context.driver = driver
 
 
